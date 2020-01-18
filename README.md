27d4d10f57bfb0d7c787542536621a6c87d6a215
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2020-01-17 16:06:22 -0800

    Fix test-dapp, add static-server (#8)
    * fix test-dapp; add static-server
    
    * add comment in deploy
    
    * add back curly braces

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 This is a simple test dapp for use in MetaMask e2e tests.
 
 Currently hosted [here](https://metamask.github.io/test-dapp/).
+
+After merging or pushing to `master`, please `npm run deploy` in the project root
+directory if any of the following files have changed:
+
+- `contract.js`
+- `index.html`
--- deploy.sh
+++ deploy.sh
@@ -20,13 +20,13 @@ function abort {
 
 function show_help {
   cat << EOF
-${__SCRIPT_NAME__}"
+${__SCRIPT_NAME__}
 Deploy site to GitHub Pages branch
 Options:
   -h, --help                    Show help text
   -s, --source <branch>         Upstream branch (defaults to 'master')
   -d, --destination <branch>    Branch to deploy to (defaults to 'gh-pages')
-  -r, --remote <remote>          Remote to deploy to (defaults to 'origin')
+  -r, --remote <remote>         Remote to deploy to (defaults to 'origin')
 EOF
 }
 
@@ -37,10 +37,10 @@ function is_working_tree_dirty {
 # for gh-pages, use unpkg.com for dependencies
 function replace_onboarding_src {
   local local_src="node_modules/@metamask/onboarding/dist/metamask-onboarding.bundle.js"
-  local web_src="unpkg.com/metamask-onboarding"
+  local web_src="https://unpkg.com/@metamask/onboarding"
   local target_file="index.html"
 
-  sed -i "" -e "s/${local_src}/${web_src}/g" "${target_file}"
+  sed -i "" -e "s#${local_src}#${web_src}#g" "${target_file}"
 }
 
 function preprocess_and_publish {
@@ -53,8 +53,8 @@ function preprocess_and_publish {
     git checkout "${DEPLOY_BRANCH}" || abort "Failed to check out ${DEPLOY_BRANCH}"
     git reset --hard "${GH_REMOTE}/${DEPLOY_BRANCH}" || abort "Failed to reset to '${GH_REMOTE}/${DEPLOY_BRANCH}'"
   else
-    git checkout -t "${GH_REMOTE}/${DEPLOY_BRANCH}" || abort "Failed to checkout '${GH_REMOTE}/${DEPLOY_BRANCH}'"
-    git rm -r ./* || abort "Failed to clean git index"
+    git checkout -b "${DEPLOY_BRANCH}" || abort "Failed to checkout '${GH_REMOTE}/${DEPLOY_BRANCH}'"
+    git rm -r ./\* || abort "Failed to clean git index"
   fi
   git checkout "${SOURCE_BRANCH}" -- contract.js index.html || abort "Failed to checkout files from '${SOURCE_BRANCH}'"
 
@@ -62,21 +62,23 @@ function preprocess_and_publish {
   replace_onboarding_src || abort "Failed to replace onboarding script source"
 
   # compute shorthash
-  local _hash
+  local shorthash
   
-  _hash=$(git show-ref "refs/heads/${SOURCE_BRANCH}")
+  shorthash=$(git show-ref "refs/heads/${SOURCE_BRANCH}")
 
-  if ! _hash
+  # the full hash is 40 bytes long, and the ref will contain at least that
+  if [ ${#shorthash} -lt 40 ]
   then
     abort "Source branch '${SOURCE_BRANCH}' has no head"
   fi
 
-  _hash="${_hash:6}"
+  shorthash="${shorthash:6}"
 
   # commit to destination branch with shorthash in message
-  git commit -am "update using ${SOURCE_BRANCH}/${_hash}" || abort "Failed to commit to destination branch '${DEPLOY_BRANCH}'"
+  git commit -am "update using ${SOURCE_BRANCH}/${shorthash}" || abort "Failed to commit to destination branch '${DEPLOY_BRANCH}'"
 
-  git push "${GH_REMOTE}" "${DEPLOY_BRANCH}" || abort "Failed to push to '${GH_REMOTE}/${DEPLOY_BRANCH}'"
+  # the "-u" here is if the branch was created
+  git push -u "${GH_REMOTE}" "${DEPLOY_BRANCH}" || abort "Failed to push to '${GH_REMOTE}/${DEPLOY_BRANCH}'"
   echo "Successfully pushed to ${GH_REMOTE}/${DEPLOY_BRANCH}"
 }
 
@@ -89,7 +91,7 @@ function main {
         exit
         ;;
       -s|--source)
-        if [[ -z $2 ]]; then
+        if [[ -z ${2} ]]; then
           printf "'source' option requires an argument.\\n" >&2
           printf "%s\\n" "${__SEE_HELP_MESSAGE__}" >&2
           exit 1
@@ -98,7 +100,7 @@ function main {
         shift
         ;;
       -d|--destination)
-        if [[ -z $2 ]]; then
+        if [[ -z ${2} ]]; then
           printf "'destination' option requires an argument.\\n" >&2
           printf "%s\\n" "${__SEE_HELP_MESSAGE__}" >&2
           exit 1
@@ -107,7 +109,7 @@ function main {
         shift
         ;;
       -r|--remote)
-        if [[ -z $2 ]]; then
+        if [[ -z ${2} ]]; then
           printf "'remote' option requires an argument.\\n" >&2
           printf "%s\\n" "${__SEE_HELP_MESSAGE__}" >&2
           exit 1
@@ -131,4 +133,4 @@ function main {
   exit 0
 }
 
-main "${@}"
+main "$@"
