7bb268fe980a4f06586b050fdf7790abd2a926f4
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2020-01-21 18:24:59 -0330

    Use shared MetaMask ESLint config (#11)

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  parserOptions: {
+    'sourceType': 'module',
+    'ecmaVersion': 2017,
+    'ecmaFeatures': {
+      'experimentalObjectRestSpread': true,
+      'impliedStrict': true,
+      'modules': true,
+      'blockBindings': true,
+      'arrowFunctions': true,
+      'objectLiteralShorthandMethods': true,
+      'objectLiteralShorthandProperties': true,
+      'templateStrings': true,
+      'classes': true,
+    },
+  },
+
+  env: {
+    'browser': true,
+  },
+
+  plugins: [
+    'json',
+  ],
+
+  extends: [
+    '@metamask/eslint-config',
+    '@metamask/eslint-config/config/nodejs',
+  ],
+
+  globals: {
+    'web3': true,
+  },
+}
