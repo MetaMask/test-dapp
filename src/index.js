MetaMask/test-dapp

commit 05d94622b24c508b06ff2e15a38678ab48ed764f

Use webpack (#63)
* add webpack, webpack-dev-server

src/index.js
@@ -28,7 +28,8 @@ The `piggybankContract` is compiled from:
   }
 */
 
-const sigUtil = require('eth-sig-util')
+import sigUtil from 'eth-sig-util'
+import MetamaskOnboarding from '@metamask/onboarding'
 
 const currentUrl = new URL(window.location.href)
 const forwarderOrigin = currentUrl.hostname === 'localhost'

.eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
   globals: {
     'web3': 'readonly',
     'ethereum': 'readonly',
-    'MetamaskOnboarding': 'readonly',
   },
   plugins: [
     'json',
@@ -17,8 +16,14 @@ module.exports = {
     '@metamask/eslint-config',
     '@metamask/eslint-config/config/nodejs',
   ],
+  overrides: [{
+    'files': ['src/index.js'],
+    'parserOptions': {
+      'sourceType': 'module',
+    },
+  }],
   ignorePatterns: [
     '!.eslintrc.js',
-    'website',
+    'dist',
   ],
 }

.gitignore
@@ -1,4 +1,4 @@
 package-lock.json
 node_modules
 .vscode
-website
+dist

build.sh
@@ -1,16 +1,0 @@
-#!/usr/bin/env bash
-
-set -u
-set -e
-set -o pipefail
-
-mkdir -p ./website/scripts
-rm -rf ./website/scripts/*
-
-cp ./node_modules/@metamask/onboarding/dist/metamask-onboarding.bundle.js ./website/scripts
-
-yarn bundle # src/index.js
-
-cp ./src/index.html ./website
-cp ./src/index.css ./website
-cp ./src/metamask-fox.svg ./website

deploy.sh
@@ -9,7 +9,7 @@ readonly __SEE_HELP_MESSAGE__="See '${__SCRIPT_NAME__} --help' for more informat
 GH_REMOTE='origin'
 SOURCE_BRANCH='master'
 DEPLOY_BRANCH='gh-pages'
-WEBSITE_DIR_PATH='website'
+WEBSITE_DIR_PATH='dist'
 RETURN_PATH='..'
 
 function abort {

src/index.html
@@ -324,7 +324,6 @@
       </section>
     </main>
 
-    <script src="./scripts/metamask-onboarding.bundle.js" defer></script>
-    <script src="index.js" defer></script>
+    <script src="bundle.js" defer></script>
   </body>
 </html>

webpack.config.js
@@ -1,0 +1,36 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+const DIST = path.resolve(__dirname, 'dist')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: DIST,
+    publicPath: DIST,
+  },
+  devServer: {
+    contentBase: DIST,
+    port: 9011,
+    writeToDisk: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+
+    // for build scripts
+    new CopyPlugin({
+      patterns: [
+        {
+          flatten: true,
+          from: './src/*',
+          globOptions: {
+            ignore: ['**/*.js'],
+          },
+        },
+      ],
+    }),
+  ],
+}

commit e6fcaa1cbbce3e9119bc099eb94260c697403e82

Fix eth-sig-util import
The `eth-sig-util` import was broken in #63 because it switch from
using `require` to `import`. `eth-sig-util` has no default export, so
the `sigUtil` variable was getting assigned to `undefined`.

The named export is now used.

src/index.js
@@ -28,7 +28,7 @@ The `piggybankContract` is compiled from:
   }
 */
 
-import sigUtil from 'eth-sig-util'
+import { encrypt } from 'eth-sig-util'
 import MetaMaskOnboarding from '@metamask/onboarding'
 
 const currentUrl = new URL(window.location.href)
@@ -471,7 +471,7 @@ const initialize = async () => {
     encryptButton.onclick = () => {
       try {
         ciphertextDisplay.innerText = web3.toHex(JSON.stringify(
-          sigUtil.encrypt(
+          encrypt(
             encryptionKeyDisplay.innerText,
             { 'data': encryptMessageInput.value },
             'x25519-xsalsa20-poly1305',