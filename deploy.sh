63f6fc46b168ae928c9cccd5a5bc0a9dbb13984e
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2021-03-24 12:42:53 -0700

    Use gh-pages package for deployment (#98)
    * Use gh-pages package for deploying
    * Refactor deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,14 @@
 set -u
 set -o pipefail
 
+GH_REMOTE="origin"
+SOURCE_BRANCH="main"
+DEPLOY_BRANCH="gh-pages"
+readonly WEBSITE_DIR_PATH="dist"
+
+# CLI constants
 readonly __SCRIPT_NAME__="${0##*/}"
 readonly __SEE_HELP_MESSAGE__="See '${__SCRIPT_NAME__} --help' for more information."
-
-GH_REMOTE='origin'
-SOURCE_BRANCH='main'
-DEPLOY_BRANCH='gh-pages'
-WEBSITE_DIR_PATH='dist'
-RETURN_PATH='..'
-
-function abort {
-  local message="${1}"
-
-  printf "ERROR: %s\\n" "${message}" >&2
-
-  exit 1
-}
 
 function show_help {
   cat << EOF
@@ -32,51 +24,7 @@ Options:
 EOF
 }
 
-function is_working_tree_dirty {
-  ! git diff --quiet
-}
-
-function get_shorthash {
-
-  local shorthash
-
-  shorthash=$(git rev-parse --short "refs/heads/${SOURCE_BRANCH}")
-
-  # the shorthash is 7 characters long
-  if [ ${#shorthash} -lt 7 ]
-  then
-    abort "git rev-parse returned an invalid shorthash from branch '${SOURCE_BRANCH}'"
-  fi
-
-  echo "${shorthash}"
-}
-
-function preprocess_and_publish {
-
-  yarn lint || abort "Lint failure"
-  yarn build || abort "Failed to build"
-
-  local shorthash
-  shorthash=$(get_shorthash)
-
-  local remote_url
-  remote_url=$(git remote get-url "${GH_REMOTE}")
-
-  cd ${WEBSITE_DIR_PATH} || abort "Failed to navigate to website directory"
-
-  git init -b ${SOURCE_BRANCH} || abort "Failed to initialize git repository in website directory"
-  git add -A . || abort "Failed to stage website files"
-  git commit -m "update using ${SOURCE_BRANCH}/${shorthash}" || abort "Failed to commit website files"
-  git push -f "${remote_url}" ${SOURCE_BRANCH}:${DEPLOY_BRANCH} || abort "Failed to push to ${GH_REMOTE}/${DEPLOY_BRANCH}"
-  echo "Successfully pushed to ${GH_REMOTE}/${DEPLOY_BRANCH}"
-
-  rm -rf .git || abort "Failed to delete .git folder in ${WEBSITE_DIR_PATH}"
-
-  cd ${RETURN_PATH} || abort "Failed to navigate back to root directory"
-}
-
-function main {
-
+function cli {
   while :; do
     case "${1-default}" in
       -h|--help)
@@ -116,14 +64,56 @@ function main {
 
     shift
   done
-
-  if is_working_tree_dirty
-  then
-    abort 'Working tree is dirty; please clean it up and try again'
-  fi
-
-  preprocess_and_publish
-  exit 0
 }
 
-main "$@"
+function is_working_tree_dirty {
+  ! git diff --quiet
+}
+
+function abort {
+  local message="${1}"
+
+  printf "ERROR: %s\\n" "${message}" >&2
+
+  exit 1
+}
+
+function get_shorthash {
+  local shorthash
+
+  shorthash=$(git rev-parse --short "refs/heads/${SOURCE_BRANCH}")
+
+  # the shorthash is 7 characters long
+  if [ ${#shorthash} -lt 7 ]
+  then
+    abort "git rev-parse returned an invalid shorthash from branch '${SOURCE_BRANCH}'"
+  fi
+
+  echo "${shorthash}"
+}
+
+function preprocess_and_deploy {
+  yarn lint || abort "Lint failure"
+  yarn build || abort "Failed to build"
+
+  local shorthash
+  shorthash=$(get_shorthash)
+
+  local commitMessage="Update using ${SOURCE_BRANCH}/${shorthash}"
+
+  yarn gh-pages --dist "$WEBSITE_DIR_PATH" --message "$commitMessage" || abort "gh-pages failed to deploy"
+
+  echo "Successfully pushed to ${GH_REMOTE}/${DEPLOY_BRANCH}"
+}
+
+# main program
+
+cli "$@"
+
+if is_working_tree_dirty
+then
+  abort "Working tree is dirty; please clean it up and try again"
+fi
+
+preprocess_and_deploy
+exit 0
