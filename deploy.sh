1b0ccfd357bc6469986e1023f5fd975e27c40106
Erik Marks <rekmarks@protonmail.com>
2021-03-24 12:14:56 -0700

    Restore CLI

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,72 @@
 set -u
 set -o pipefail
 
-readonly GH_REMOTE="origin"
-readonly SOURCE_BRANCH="main"
-readonly DEPLOY_BRANCH="gh-pages"
+GH_REMOTE="origin"
+SOURCE_BRANCH="main"
+DEPLOY_BRANCH="gh-pages"
 readonly WEBSITE_DIR_PATH="dist"
+
+# CLI constants
+readonly __SCRIPT_NAME__="${0##*/}"
+readonly __SEE_HELP_MESSAGE__="See '${__SCRIPT_NAME__} --help' for more information."
+
+function show_help {
+  cat << EOF
+${__SCRIPT_NAME__}
+Deploy site to GitHub Pages branch
+Options:
+  -h, --help                    Show help text
+  -s, --source <branch>         Upstream branch (defaults to 'main')
+  -d, --destination <branch>    Branch to deploy to (defaults to 'gh-pages')
+  -r, --remote <remote>         Remote to deploy to (defaults to 'origin')
+EOF
+}
+
+function cli {
+  while :; do
+    case "${1-default}" in
+      -h|--help)
+        show_help
+        exit
+        ;;
+      -s|--source)
+        if [[ -z $2 ]]; then
+          printf "'source' option requires an argument.\\n" >&2
+          printf "%s\\n" "${__SEE_HELP_MESSAGE__}" >&2
+          exit 1
+        fi
+        SOURCE_BRANCH="${2}"
+        shift
+        ;;
+      -d|--destination)
+        if [[ -z $2 ]]; then
+          printf "'destination' option requires an argument.\\n" >&2
+          printf "%s\\n" "${__SEE_HELP_MESSAGE__}" >&2
+          exit 1
+        fi
+        DEPLOY_BRANCH="${2}"
+        shift
+        ;;
+      -r|--remote)
+        if [[ -z $2 ]]; then
+          printf "'remote' option requires an argument.\\n" >&2
+          printf "%s\\n" "${__SEE_HELP_MESSAGE__}" >&2
+          exit 1
+        fi
+        GH_REMOTE="${2}"
+        shift
+        ;;
+      *)
+        break
+    esac
+
+    shift
+  done
+}
+
+function is_working_tree_dirty {
+  ! git diff --quiet
+}
 
 function abort {
   local message="${1}"
@@ -16,12 +78,7 @@ function abort {
   exit 1
 }
 
-function is_working_tree_dirty {
-  ! git diff --quiet
-}
-
 function get_shorthash {
-
   local shorthash
 
   shorthash=$(git rev-parse --short "refs/heads/${SOURCE_BRANCH}")
@@ -36,7 +93,6 @@ function get_shorthash {
 }
 
 function preprocess_and_deploy {
-
   yarn lint || abort "Lint failure"
   yarn build || abort "Failed to build"
 
@@ -51,6 +107,8 @@ function preprocess_and_deploy {
 }
 
 function main {
+  cli "$@"
+
   if is_working_tree_dirty
   then
     abort "Working tree is dirty; please clean it up and try again"
