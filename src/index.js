e3d1ff071ee3c72970d05d9940ba40c0bf214f9a
Brad Decker <git@braddecker.dev>
2021-06-25 12:26:17 -0500

    Add switchEthereumChain button (#102)
    Co-authored-by: Erik Marks <25517051+rekmarks@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -81,6 +81,7 @@ const signTypedDataV4VerifyResult = document.getElementById('signTypedDataV4Veri
 
 // Miscellaneous
 const addEthereumChain = document.getElementById('addEthereumChain')
+const switchEthereumChain = document.getElementById('switchEthereumChain')
 
 const initialize = async () => {
   try {
@@ -185,6 +186,7 @@ const initialize = async () => {
 
     if (isMetaMaskInstalled()) {
       addEthereumChain.disabled = false
+      switchEthereumChain.disabled = false
     } else {
       onboardButton.innerText = 'Click here to install MetaMask!'
       onboardButton.onclick = onClickInstall
@@ -213,6 +215,15 @@ const initialize = async () => {
         chainName: 'xDAI Chain',
         nativeCurrency: { name: 'xDAI', decimals: 18, symbol: 'xDAI' },
         blockExplorerUrls: ['https://blockscout.com/poa/xdai'],
+      }],
+    })
+  }
+
+  switchEthereumChain.onclick = async () => {
+    await ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{
+        chainId: '0x64',
       }],
     })
   }
