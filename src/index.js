c6fe89fc264f70df70bae6befbf5671c73d0d822
Brad Decker <git@braddecker.dev>
2021-07-08 13:36:34 -0500

    add EIP1559 tx button (#117)

--- src/index.js
+++ src/index.js
@@ -49,6 +49,7 @@ const contractStatus = document.getElementById('contractStatus');
 
 // Send Eth Section
 const sendButton = document.getElementById('sendButton');
+const sendEIP1559Button = document.getElementById('sendEIP1559Button');
 
 // Send Tokens Section
 const tokenAddress = document.getElementById('tokenAddress');
@@ -323,6 +324,23 @@ const initialize = async () => {
         value: '0x29a2241af62c0000',
         gasLimit: 21000,
         gasPrice: 20000000000,
+      });
+      console.log(result);
+    };
+
+    sendEIP1559Button.onclick = async () => {
+      const result = await ethereum.request({
+        method: 'eth_sendTransaction',
+        params: [
+          {
+            from: accounts[0],
+            to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970',
+            value: '0x29a2241af62c0000',
+            gasLimit: '0x5028',
+            maxFeePerGas: '0x2540be400',
+            maxPriorityFeePerGas: '0x3b9aca00',
+          },
+        ],
       });
       console.log(result);
     };
@@ -965,6 +983,18 @@ const initialize = async () => {
     chainIdDiv.innerHTML = chainId;
   }
 
+  function handleEIP1559Support(supported) {
+    if (supported && Array.isArray(accounts) && accounts.length >= 1) {
+      sendEIP1559Button.disabled = false;
+      sendEIP1559Button.hidden = false;
+      sendButton.innerText = 'Send Legacy Transaction';
+    } else {
+      sendEIP1559Button.disabled = true;
+      sendEIP1559Button.hidden = true;
+      sendButton.innerText = 'Send';
+    }
+  }
+
   function handleNewNetwork(networkId) {
     networkDiv.innerHTML = networkId;
   }
@@ -980,6 +1010,13 @@ const initialize = async () => {
         method: 'net_version',
       });
       handleNewNetwork(networkId);
+
+      const block = await ethereum.request({
+        method: 'eth_getBlockByNumber',
+        params: ['latest', false],
+      });
+
+      handleEIP1559Support(block.baseFeePerGas !== undefined);
     } catch (err) {
       console.error(err);
     }
@@ -991,9 +1028,32 @@ const initialize = async () => {
     ethereum.autoRefreshOnNetworkChange = false;
     getNetworkAndChainId();
 
-    ethereum.on('chainChanged', handleNewChain);
+    ethereum.autoRefreshOnNetworkChange = false;
+    getNetworkAndChainId();
+
+    ethereum.on('chainChanged', (chain) => {
+      handleNewChain(chain);
+      ethereum
+        .request({
+          method: 'eth_getBlockByNumber',
+          params: ['latest', false],
+        })
+        .then((block) => {
+          handleEIP1559Support(block.baseFeePerGas !== undefined);
+        });
+    });
     ethereum.on('networkChanged', handleNewNetwork);
-    ethereum.on('accountsChanged', handleNewAccounts);
+    ethereum.on('accountsChanged', (newAccounts) => {
+      ethereum
+        .request({
+          method: 'eth_getBlockByNumber',
+          params: ['latest', false],
+        })
+        .then((block) => {
+          handleEIP1559Support(block.baseFeePerGas !== undefined);
+        });
+      handleNewAccounts(newAccounts);
+    });
 
     try {
       const newAccounts = await ethereum.request({
