f80b16e4b1c105c6305cfb0b3bb41eee0c023feb
NidhiKJha <menidhikjha@gmail.com>
2023-02-15 15:26:40 +0530

    updated contract

--- contracts/TestDappCollectibles.sol
+++ contracts/TestDappCollectibles.sol
@@ -1,25 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.8.1/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import 'base64-sol/base64.sol';
 
-contract TestDappCollectibles is ERC721 {
+contract TestDappNfts is ERC721 {
   
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
-  constructor() ERC721('TestDappCollectibles', 'TDC') {}
+    constructor() ERC721("NFT Token", "NFT") {}
 
-  function mintCollectibles(uint numberOfTokens) public {
-    for(uint i = 1; i <= numberOfTokens; i++) {
-      _tokenIds.increment();
-      uint tokenId = _tokenIds.current();
-      _safeMint(_msgSender(), tokenId);
+    function safeMint(uint256 tokenId) public {
+        _safeMint(msg.sender, tokenId);
     }
-  }
 
   function tokenURI(uint tokenId) public pure override returns (string memory) {
     string memory svg = 
