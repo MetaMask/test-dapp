d6527d3766e381d88cd912fde3eccabc4a74e2e7
seaona <mariona@gmx.es>
2023-12-12 11:58:14 +0100

    wc support

---
+++ src/onchain-sample-contracts.js
@@ -0,0 +1,24 @@
+// Contracts for Malicious Transactions in different chains
+export const NETWORKS_BY_CHAIN_ID = {
+  137: 'polygon',
+  10: 'optimism',
+  56: 'bsc',
+  43114: 'avalanche',
+  42161: 'arbitrum',
+};
+
+export const ERC20_SAMPLE_CONTRACTS = {
+  polygon: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174',
+  optimism: '0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85',
+  bsc: '0x8965349fb649A33a30cbFDa057D8eC2C48AbE2A2',
+  avalanche: '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E',
+  arbitrum: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
+};
+
+export const ERC721_SAMPLE_CONTRACTS = {
+  polygon: '0x9e8ea82e76262e957d4cc24e04857a34b0d8f062',
+  optimism: '0xaf18644083151cf57f914cccc23c42a1892c218e',
+  bsc: '0xebfbfd7c41b123500fb16b71c43b400c12b08be0',
+  avalanche: '0x880f7e04D722e305126F7E1efd3434A7d5b1465c',
+  arbitrum: '0x8659a4876369b94515a86048fe7f99daba6b9a7d',
+};
