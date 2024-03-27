82481b082d07b746ab2933d9a50d36f3d0dfcd43
sleepytanya <104780023+sleepytanya@users.noreply.github.com>
2024-03-27 10:40:23 -0400

    fix contract address

--- src/onchain-sample-contracts.js
+++ src/onchain-sample-contracts.js
@@ -26,5 +26,5 @@ export const ERC721_SAMPLE_CONTRACTS = {
   avalanche: '0x880f7e04D722e305126F7E1efd3434A7d5b1465c',
   arbitrum: '0x8659a4876369b94515a86048fe7f99daba6b9a7d',
   sepolia: '0xbba60aa8144579e07c6db64121b0f608ab6f0c89',
-  base: '0xc2a953B9c7a9FD458b1A6CdF17f61D828efE4bc5',
+  base: '0xc2106ca72996e49bBADcB836eeC52B765977fd20',
 };
